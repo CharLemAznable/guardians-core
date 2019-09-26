13203e44416d3873bfb01c55fa3c148b6a6fc07f
CharLemAznable <545541819@qq.com>
2019-09-26 16:37:09 +0800

    Upgrade, some refactor.

--- src/main/java/com/github/charlemaznable/guardians/spring/GuardiansConfigurer.java
+++ src/main/java/com/github/charlemaznable/guardians/spring/GuardiansConfigurer.java
@@ -1,19 +1,18 @@
 package com.github.charlemaznable.guardians.spring;
 
-import com.github.charlemaznable.core.spring.ComplexBeanNameGenerator;
+import com.github.charlemaznable.core.spring.ComplexComponentScan;
 import com.github.charlemaznable.core.spring.ComplexImport;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.lang.NonNull;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SuppressWarnings("SpringFacetCodeInspection")
-@ComponentScan(nameGenerator = ComplexBeanNameGenerator.class)
 @Configuration
 @ComplexImport
-public class GuardiansConfiguration implements WebMvcConfigurer {
+@ComplexComponentScan
+public class GuardiansConfigurer implements WebMvcConfigurer {
 
     @Autowired
     private GuardiansInterceptor guardiansInterceptor;
--- src/main/java/com/github/charlemaznable/guardians/spring/GuardiansImport.java
+++ src/main/java/com/github/charlemaznable/guardians/spring/GuardiansImport.java
@@ -11,6 +11,6 @@ import java.lang.annotation.Target;
 @Documented
 @Target({ElementType.TYPE})
 @Retention(RetentionPolicy.RUNTIME)
-@Import(GuardiansConfiguration.class)
+@Import(GuardiansConfigurer.class)
 public @interface GuardiansImport {
 }
