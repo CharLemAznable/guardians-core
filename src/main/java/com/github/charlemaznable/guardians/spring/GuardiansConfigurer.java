95db1cbd70617217843873a70365357b17621cec
CharLemAznable <545541819@qq.com>
2019-12-04 12:50:31 +0800

    Fixed @NonNull/@Nullable annotations.

--- src/main/java/com/github/charlemaznable/guardians/spring/GuardiansConfigurer.java
+++ src/main/java/com/github/charlemaznable/guardians/spring/GuardiansConfigurer.java
@@ -4,9 +4,10 @@ import com.github.charlemaznable.core.spring.ComplexComponentScan;
 import com.github.charlemaznable.core.spring.ComplexImport;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.lang.NonNull;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import javax.annotation.Nonnull;
 
 @SuppressWarnings("SpringFacetCodeInspection")
 @Configuration
@@ -18,7 +19,7 @@ public class GuardiansConfigurer implements WebMvcConfigurer {
     private GuardiansInterceptor guardiansInterceptor;
 
     @Override
-    public void addInterceptors(@NonNull InterceptorRegistry registry) {
+    public void addInterceptors(@Nonnull InterceptorRegistry registry) {
         registry.addInterceptor(guardiansInterceptor);
     }
 }
